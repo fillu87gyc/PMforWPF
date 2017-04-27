201515b0bb77ec05c5e220398e3d490317e9f867
fillu87gyc <wander.off65@gmail.com>
2017-04-27 12:51:47 +0900

    ファイルを追加、登録画面作成へ向けてPokemonClassのコンストラクタ作成

--- PokemonManagerWPF/PokemonManage.cs
+++ PokemonManagerWPF/PokemonManage.cs
@@ -8,49 +8,31 @@ namespace PokemonManage
 {
 		public class Pokemon
 		{
-				public Pokemon()
+				public Pokemon(string NameArg)
 				{
+						var xlbook = new ClosedXML.Excel.XLWorkbook(Base.Preparation.DATABASE + "Name.xlsx");
+						int counter = 0;
+						while (NameArg != (xlbook.Worksheet(1).Cell(counter, 1).Value.ToString()))
+						{
+								counter++;
+						}
+						var row = counter;
+						for (int i = 0; i < 6; i++)
+						{
+								BaseSt[i] = int.Parse(xlbook.Worksheet(1).Cell(row, i + 3).Value.ToString());
+						}
 
+						Show.ImgType1 = xlbook.Worksheet(1).Cell(row, 10).Value.ToString();
+						Show.ImgType2 = xlbook.Worksheet(1).Cell(row, 11).Value.ToString();
+
+						Abilitys[0] = new Ability.Ability(xlbook.Worksheet(1).Cell(row, 12).Value.ToString());
+						Abilitys[1] = new Ability.Ability(xlbook.Worksheet(1).Cell(row, 13).Value.ToString());
+						Abilitys[2] = new Ability.Ability(xlbook.Worksheet(1).Cell(row, 14).Value.ToString());
 				}
-
+				StatusManager.Status St;
+				int[] BaseSt = new int[6];
+				Ability.Ability[] Abilitys = new Ability.Ability[3]; 
+				Base.Property Show = new Base.Property();
 		}
 }
-namespace StatusManager
-{
-		public class Status
-		{
-				public Status(int[] e, int[] i)
-				{
-						EffortValue = e;
-						IndividualValue = i;
-				}
-				public Status()
-				{
 
-				}
-				public int[] EffortValue
-				{
-						get
-						{
-								return effortvalue;
-						}
-						private set
-						{
-								effortvalue = value;
-						}
-				}
-				public int[] IndividualValue
-				{
-						get
-						{
-								return individualvalue;
-						}
-						private set
-						{
-								individualvalue = value;
-						}
-				}
-				int[] effortvalue = new int[6] { 0, 0, 0, 0, 0, 0 };
-				int[] individualvalue = new int[6] { 0, 0, 0, 0, 0, 0 };
-		}
-}
---
+++ PokemonManagerWPF/StatusManager.cs
@@ -0,0 +1,39 @@
+﻿namespace StatusManager
+{
+		public class Status
+		{
+				public Status(int[] e, int[] i)
+				{
+						EffortValue = e;
+						IndividualValue = i;
+				}
+				public Status()
+				{
+
+				}
+				public int[] EffortValue
+				{
+						get
+						{
+								return effortvalue;
+						}
+						private set
+						{
+								effortvalue = value;
+						}
+				}
+				public int[] IndividualValue
+				{
+						get
+						{
+								return individualvalue;
+						}
+						private set
+						{
+								individualvalue = value;
+						}
+				}
+				int[] effortvalue = new int[6] { 0, 0, 0, 0, 0, 0 };
+				int[] individualvalue = new int[6] { 0, 0, 0, 0, 0, 0 };
+		}
+}
